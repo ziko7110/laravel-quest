57c569915b13dc488e9391ff84c1f9fee4fddc47
ziko7110 <ziko7110@gmail.com>
2021-03-14 20:36:56 +0900

    topページ作成

---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,21 @@
+<header class="mb-5">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <a class="navbar-brand" href="/">YouTube-Curation</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link">新規ユーザ登録</a></li>
+                <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+</header>
